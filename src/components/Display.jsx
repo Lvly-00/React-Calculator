d769cea067266d490585e78951ee979ee0b8de7e
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-08-13 13:30:53 +0800

    project structure

---
+++ src/components/Display.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Display() {
+  return (
+    <div>
+      <h1>Display Component</h1>
+      <p>This is a simple display component.</p>
+    </div>
+  );
+}
